ba9ef8e4ea46873759b7050a9f04061fc60312f8
king-11 <lakshay.singh1108@gmail.com>
2020-08-07 16:02:42 +0530

    serializers and views updated for firebase id_token verfication

--- authentication/views.py
+++ authentication/views.py
@@ -11,13 +11,26 @@ User = get_user_model()
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
+    def login(self):
+        validated_data = self.serializer.validated_data
+        self.user = validated_data['user']
+        # self.verification_status = validated_data['is_verified']
+        self.token = create_auth_token(self.user)
+
+    def get_response(self):
+        response = Response({
+            # 'user_id': self.user.pk,
+            'token': self.token,
+            # 'verification_status':self.verification_status,
+            # 'is_profile_complete':self.is_profile_complete,
+        })
+        return Response(response.data, status=status.HTTP_200_OK)
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data['user']
-            token = create_auth_token(user)
-            response = ResponseSerializer({'token': token})
-            return Response(response.data, status.HTTP_200_OK)
+            self.login()
+            return self.get_response()
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -26,13 +39,23 @@ class RegisterView(generics.CreateAPIView):
     model = User
     serializer_class = RegisterSerializer
 
+    def perform_create(self, serializer):
+        self.user = serializer.save()
+        self.token = create_auth_token(user=serializer.instance)
+
+    def get_response(self):
+        response = ResponseSerializer({
+            'token': self.token,
+            # 'verification_status':self.user.verified_account.is_verified,
+        })
+        return Response(response.data, status=status.HTTP_201_CREATED)
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        token = create_auth_token(user=serializer.instance)
-        response = ResponseSerializer({'token': token})
-        return Response(response.data, status.HTTP_201_CREATED)
+
+        return self.get_response()
 
 
 class UserView(generics.RetrieveAPIView):
