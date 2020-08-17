b0c6177993a1cc88949c8c9ad484b347b4ac9eff
king-11 <lakshay.singh1108@gmail.com>
2020-08-17 20:32:20 +0530

    url for like view added

--- image_store/urls.py
+++ image_store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
+
 from .views import *
 urlpatterns = [
     path('team/', TeamView.as_view()),
     path('', ImagesView.as_view()),
     path('image/<int:pk>/', ImageView.as_view()),
     path('profile/<int:pk>/', ProfileView.as_view()),
+    path('image/like/', LikeUpdateView.as_view())
 ]
