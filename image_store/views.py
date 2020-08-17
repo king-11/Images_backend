9b41a92abbcb4b7490ac63cd8e6aa7847ff167b2
king-11 <lakshay.singh1108@gmail.com>
2020-08-17 15:57:13 +0530

    images view updated

--- image_store/views.py
+++ image_store/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, status, authentication
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
 from .serializer import *
@@ -22,8 +22,17 @@ class ImagesView(generics.ListCreateAPIView):
     serializer_class = ImageSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+    def create(self, request):
+        self.request = request
+        self.serializer = self.get_serializer(
+            data=request.data, context={'request': request})
+        self.serializer.is_valid(raise_exception=True)
+
+        self.serializer.create()
+        return Response(status=status.HTTP_201_CREATED)
+
 
 class ImageView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = images.objects.filter()
+    queryset = images.objects.all()
     serializer_class = ImageSerializer
     permission_classes = (IsOwnerOrReadOnlyImages,)
