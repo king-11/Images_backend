a3d6dc01f0dddae09e38a924a97e935695e8d0ba
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-03 18:45:30 +0530

    chore: connect to postgres db

--- wallpaper/settings.py
+++ wallpaper/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'wallpaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv('NAME'),
+        "USER": os.getenv('USERNAME'),
+        "PASSWORD": os.getenv('PASSWORD'),
+        "HOST": os.getenv('HOST'),
+        "PORT": os.getenv('PORT')
     }
 }
 
